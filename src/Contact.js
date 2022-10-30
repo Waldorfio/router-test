d7b3cccf8c4d0effa0d002af75200906c4857d7f
Walid VM <walid.hanifi707@gmail.com>
2022-10-30 20:30:54 +1000

    Initial commit

---
+++ src/Contact.js
@@ -0,0 +1,11 @@
+const Contact = () => {
+    return (
+      <div>
+        <h1>Contact Info Page!</h1>
+        <a href='/'>Return Home</a>
+      </div>
+    );
+  };
+  
+  export default Contact;
+  
